09aa59c4da2a16b23e523b6f9d8473054d991f93
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-06-15 14:29:09 -0500

    Overflow agregado

---
+++ simbolo.model.js
@@ -0,0 +1,31 @@
+export class Simbolo {
+    simbolo
+    #elementoHTML
+
+    constructor(simbolo) {
+        this.simbolo = simbolo
+    }
+
+    iniciarElemento() {
+        let bitContainer = document.createElement('div')
+        bitContainer.classList.add('bit-container')
+        bitContainer.innerHTML = `
+        <div class="bit-simbolo">${this.simbolo}</div>
+        <div class="cabezal-container">
+            <img class="cabezal-element" src="./resources/img/triangle.png" alt="triangle" class="pos_cabezal">
+        </div>`
+        this.#elementoHTML = bitContainer
+    }
+
+    desactivarCabezal() {
+        this.#elementoHTML.querySelector('.cabezal-element').classList.toggle('pos_cabezal')
+    }
+
+    activarCabezal() {
+        this.#elementoHTML.querySelector('.cabezal-element').classList.toggle('pos_cabezal')
+    }
+
+    cambiarSimbolo(nuevoSimbolo) {
+        
+    }
+}
