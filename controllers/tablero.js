598d59711933bceed542ab95d2f5fb963e2b0ae8
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-06-15 16:26:22 -0500

    Clase MT creada

--- controllers/tablero.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Tablero = void 0;
-class Tablero {
-    constructor() {
-        console.log('Funciona desde el tablero');
-    }
-}
-exports.Tablero = Tablero;
